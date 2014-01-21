9b61f43ce61137b727225cf6eba9f4b85fa5a960
fbeaudet <fredericbeaudet@gmail.com>
2014-01-21 16:55:53 -0500

    initial code commit

--- ariko.c
+++ ariko.c
@@ -15,12 +15,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * -------------------------------------------------------------------------
- *
- * - Compile with gcc: gcc ariko.c -std=c99 -o "ariko" 
- * - Use like this: ariko "filepath.rom" "words to search" 8/16 [little/big]
- *                                                        (bits)(endianness)
  */
 #include <ctype.h>
 #include <fcntl.h>
