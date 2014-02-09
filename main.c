012a5eb9c7acf5dad945ab12ebece517aff51527
fbeaudet <fredericbeaudet@gmail.com>
2014-02-09 01:03:06 -0500

    minor.

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * Page: https://github.com/fbeaudet/ariko
  * Repository: https://github.com/fbeaudet/ariko.git
  */
 
